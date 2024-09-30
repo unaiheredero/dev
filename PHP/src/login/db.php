9978e8c6cccc6a31f0d0149929e0e504ec55bfe7
unai <uherederoji24dw@ikzubirimanteo.com>
2024-09-30 11:11:27 +0200

    [NEW] added new fitchers

---
+++ PHP/src/login/db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "db";  // docker-compose.yml begiratu
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
